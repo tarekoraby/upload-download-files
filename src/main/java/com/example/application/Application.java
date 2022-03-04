29b9b8bc01157b0c48002a78f392054d45cd4362
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-03-04 15:09:51 +0200

    Update to Vaadin 23.0.0

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -3,7 +3,6 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.vaadin.artur.helpers.LaunchUtil;
 import com.vaadin.flow.component.dependency.NpmPackage;
 
 /**
